fea956f2f18611371049825ca4dfdad2b69cc408
Ashish Rout <ashishrout356@gmail.com>
2026-02-25 02:04:12 +0530

    apis added

--- Spotify_Project_Backend/Src/Controllers/Music.controller.js
+++ Spotify_Project_Backend/Src/Controllers/Music.controller.js
@@ -1,7 +1,7 @@
 const musicModel = require('../Models/Music.models')
 const jwt = require('jsonwebtoken')
 const {uploadFile}  = require('../Services/Storage.services')
-
+const albumModel = require('../Models/Album.models')
 
 async function createMusic(req, res){
 
@@ -51,4 +51,46 @@ async function createMusic(req, res){
 
 } 
 
-module.exports = {createMusic}
+
+async function createAlbum(req, res){
+    const token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        if(decoded.role !== "artist"){
+            return res.status(401).json({
+                message: "you do not have acces this page"
+            })
+        }
+       const {title, musics } = req.body;
+
+        const album = await albumModel.create({
+            title,
+            musics:musics,
+            artist: decoded.id,
+        })
+        res.status(201).json({
+            message: 'Album created successfully',
+            album:{
+                id: album._id,
+                title: album.title,
+                musics: album.musics,
+                artist: album.artist
+            }
+        })
+    }
+    catch(err){
+        console.log(err);
+        res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+}
+
+
+
+module.exports = {createMusic, createAlbum}
--- Spotify_Project_Backend/Src/Routes/Music.routes.js
+++ Spotify_Project_Backend/Src/Routes/Music.routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const musicControllers = require('../Controllers/Music.controller')
+
 const router = express.Router()
 const multer = require('multer')
 
 const upload  = multer()
 
 router.post('/create-music',upload.single('music'), musicControllers.createMusic)
+
+router.post('/create-album', musicControllers.createAlbum)
 
 
 module.exports = router;
